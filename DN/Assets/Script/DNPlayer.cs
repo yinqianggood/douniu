31223f325e5bba20071e34963bc5176223420e3d
yinqianggood <yinqianggood@qq.com>
2017-07-11 10:14:57 +0800

    sdfsdf

---
+++ DN/Assets/Script/DNPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using DG.Tweening;
+using UnityEngine.UI;
+using System;
+public class DNPlayer : MonoBehaviour {
+
+    public Image avatarIcon;
+    public Text avatarName;
+    public Text avatarScores;
+    public GameObject handCardsParent;
+    public GameObject handCardPrefab;
+    public List<GameObject>handCards = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
